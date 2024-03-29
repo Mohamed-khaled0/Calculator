6233c50909995bd7d9f630e898c3301c6a12b653
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-03-29 17:36:59 +0200

    Refactore

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -10,33 +10,51 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     private var isFinishedTypingNumber:Bool = true
+    private var displayValue:Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Can't convert display label text to a double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+            
+        }
+    }
     
+
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Can't convert display label text to a double.")
-        }
         if let calcMethod = sender.currentTitle {
-            if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
-            }else if  calcMethod == "AC"{
-                displayLabel.text = "0"
-            }else if calcMethod == "%" {
-                displayLabel.text = String(number * 0.01)
-            }
-        }
-    }
+            let calculator = CalculatorLogic(number: displayValue)
+                  if let result = calculator.calculate(symbol: calcMethod) {
+                      displayValue = result
+                  }
+              }
         
-        @IBAction func numButtonPressed(_ sender: UIButton) {
-            if let numValue =  sender.currentTitle {
-                if  isFinishedTypingNumber == true  {
-                    displayLabel.text = numValue
-                    isFinishedTypingNumber = false
-                }else{
-                    displayLabel.text = displayLabel.text! + numValue
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue =  sender.currentTitle {
+            if  isFinishedTypingNumber == true  {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }else{
+                if numValue == "." {
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Can't convert display label text to a double.")
+                    }
+                    let isInt = floor(Double(displayValue)) == displayValue
+                    if !isInt {
+                        return
+                    }
                 }
+                
+                displayLabel.text = displayLabel.text! + numValue
             }
         }
-        
     }
+    
+}
 
--- Calculator/Model/CalculatorModel.swift
+++ Calculator/Model/CalculatorModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct CalculatorLogic {
+    
+    var number:Double
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate (symbol: String) -> Double? {
+            if symbol == "+/-" {
+               return number * -1
+            }else if  symbol == "AC"{
+                return 0
+            }else if symbol == "%" {
+                return number * 0.01
+            }
+        return nil
+    }
+}
