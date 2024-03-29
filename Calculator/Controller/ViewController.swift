489dee1d78482a4ee5836739a01400d3cec4032e
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-03-29 16:44:35 +0200

    added basic functionality like AC button , +/- , %

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -9,20 +9,34 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-    
-    
+    private var isFinishedTypingNumber:Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Can't convert display label text to a double.")
+        }
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            }else if  calcMethod == "AC"{
+                displayLabel.text = "0"
+            }else if calcMethod == "%" {
+                displayLabel.text = String(number * 0.01)
+            }
+        }
+    }
         
-        //What should happen when a non-number button is pressed
-    
+        @IBAction func numButtonPressed(_ sender: UIButton) {
+            if let numValue =  sender.currentTitle {
+                if  isFinishedTypingNumber == true  {
+                    displayLabel.text = numValue
+                    isFinishedTypingNumber = false
+                }else{
+                    displayLabel.text = displayLabel.text! + numValue
+                }
+            }
+        }
+        
     }
 
-    
-    @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
-    }
-
-}
